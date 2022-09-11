50d4bc464c8e6437d938071fb614c1f4adaaa41d
Dylan Sapienza <dsap@tutamail.com>
2022-09-11 14:30:14 -0400

    Updated

--- B/Other/xcollects.py
+++ B/Other/xcollects.py
@@ -5,16 +5,28 @@ acceptable_strings = ["┘", "┐", "└", "┌"]
 unacceptable_output = 'unacceptable input'
 
 
-def acceptable(input_str):
+def acceptable(input_str: str) -> bool:
+    """
+    Parameters:
+    input_str: given string
+
+    Returns boolean
+
+    acceptable represents a function to check if a given string is a acceptable string.
+    """
+
     return input_str in acceptable_strings
 
 
-def unacceptable():
-    print(unacceptable_output)
-    exit(1)
+def xcollects() -> int:
+    """
+    Parameters: None
 
+    Returns exit code
 
-def main():
+    xcollects reads a stream of lines from STDIN and prints a single string on a line by itself to STDOUT of all the valid STDIN characters.
+    """
+
     return_str = ''
     while True:
         try:
@@ -22,7 +34,8 @@ def main():
             if acceptable(input_str):
                 return_str += input_str
             else:
-                unacceptable()
+                print(unacceptable_output)
+                exit(1)
 
         # ending successfully (on user-break with 'CTRL-D')
         except SyntaxError:
@@ -31,4 +44,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    xcollects()
