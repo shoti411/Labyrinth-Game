74c58953e66a8c72ad2d1d6e4dd64b29cca6bda2
Thomas Crook <crook.t@northeastern.edu>
2022-09-19 13:26:07 -0400

    D-2 done.

--- D/Other/xgui.py
+++ D/Other/xgui.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import json
 
+# The dimensions of a single image tile in the gui
 tile_dimensions = (100, 100)
 
+# Mapping acceptable characters to their corresponding file path
 file_map = {
     "┌": './Images/right-down.png',
     "┐": './Images/left-down.png',
@@ -13,12 +15,22 @@ file_map = {
 
 
 def read_stdin():
+    """
+    Parses the stdin stream into distinct rows of acceptable characters
+    :return: rows: (List-of String) -> Represents the acceptable characters that were parsed
+    """
     rows = []
     while True:
         try:
-            input_str = input()
-            if input_str != "":
-                rows.append(input_str)
+            input_str = input().strip()
+            input_rows = [x.strip() for x in input_str.split('\"')]
+
+            while "" in input_rows:
+                input_rows.remove('')
+
+            for row in input_rows:
+                rows.append(row)
+
         # ending successfully (on user-break with 'CTRL-D') or End of File
         except EOFError:
             break
@@ -26,31 +38,44 @@ def read_stdin():
 
 
 def get_image(character):
+    """
+    Return the correct image, given an acceptable character
+    :param character: (String) Acceptable character
+    :return: im: (Image) The mapped acceptable character's image
+    """
     im = Image.open(file_map[character])
     im = ImageTk.PhotoImage(im)
     return im
 
 
 def handle_mouse_click(event):
-    y = event.widget.grid_info()['row']
-    x = event.widget.grid_info()['column']
-    out_str = json.dumps([x, y])
+    """
+    Prints click location, and exits program on click event
+    :param event: (ButtonPress event) left mouse click event
+    """
+    out_str = json.dumps([event.x, event.y])
     print(out_str)
     exit()
 
 
 def xgui(input_arr):
+    """
+    Creates gui effect representing the parsed rows of acceptable characters from STDIN
+    :param input_arr: (List-of String) -> The parsed rows of acceptable characters
+    """
     window = tk.Tk()
-    w, h = (tile_dimensions[0] * len(input_arr[0]),
-            tile_dimensions[1] * len(input_arr))
-    window.geometry(f'{w}x{h}')
     window.bind("<Button-1>", handle_mouse_click)
-    for i in range(len(input_arr)):
-        for j in range(len(input_arr[0])):
-            im = get_image(input_arr[i][j])
-            panel = tk.Label(window, image=im, bd=0)
-            panel.image = im
-            panel.grid(row=i, column=j)
+
+    if input_arr:
+        w, h = (tile_dimensions[0] * len(input_arr[0]),
+                tile_dimensions[1] * len(input_arr))
+        window.geometry(f'{w}x{h}')
+        for i in range(len(input_arr)):
+            for j in range(len(input_arr[0])):
+                im = get_image(input_arr[i][j])
+                panel = tk.Label(window, image=im, bd=0)
+                panel.image = im
+                panel.grid(row=i, column=j)
 
     window.mainloop()
 
---
+++ D/README.md
@@ -0,0 +1,9 @@
+<strong>README.md: </strong> Readme filled with information about the project</br></br>
+<strong> Other/xgui.py: </strong></br> 
+Python script that takes stream of well-formed JSON strings from STDIN, parses and then presents a GUI representing the acceptable strings as images.</br></br>
+<strong>xgui: </strong> Bash script</br></br>
+
+<strong> RoadMap: </strong></br>
+<li>Auxilary files are in D/Other. There is only 1 auxilary file contained in other which is xgui.py which is the python script stated above.</li>
+<li>Image files are in D/Images. The files are image representations of the acceptable strings.</li></br>
+<li>In D directory contains the README.md about the project and the xgui bash script for user to call.</li></br>
