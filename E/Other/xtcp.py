8a2148be71c163afee57a407f4669122615ffb0f
Thomas Crook <crook.t@northeastern.edu>
2022-09-22 12:27:32 -0400

    testing on different machine

--- E/Other/xtcp.py
+++ E/Other/xtcp.py
@@ -3,6 +3,13 @@ import socket
 
 if __name__ == "__main__":
     port = int(12345)
-    server_address = ('localhost', port)
-    socket.create_connection(server_address)
-
+    server_address = ('127.0.0.1', port)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(server_address)
+        s.listen()
+        conn, addr = s.accept()
+        data = ''
+        while True:
+            data += str(conn.recv(1024))
+            if not data:
+                break
