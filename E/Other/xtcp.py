c0126c2032f415317d3ed9d4fda5651a48753e61
Dsap5131 <loldsap5131@gmail.com>
2022-09-26 13:36:18 -0400

    test

--- E/Other/xtcp.py
+++ E/Other/xtcp.py
@@ -57,6 +57,7 @@ def xtcp():
     conn, host = connect_tcp()
     data = collect_input(connection=conn)
     processed_data = xjson.xjson(data)
+    processed_data += '\n'
     conn.send(bytes(json.dumps(processed_data, ensure_ascii=False), 'utf-8'))
     host.close()
 
