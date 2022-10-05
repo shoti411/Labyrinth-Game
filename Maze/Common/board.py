7e375579fe93a082ccb6e4119fc870eec01cacc0
Thomas Crook <crook.t@northeastern.edu>
2022-10-05 13:34:08 -0400

    update

---
+++ Maze/Common/board.py
@@ -0,0 +1,10 @@
+"""
+
+sliding a designated row or column in one or the other direction;
+
+inserting a tile into the spot that is left open by a slide;
+
+determining which tiles are reachable from a designated tile.
+
+"""
+
