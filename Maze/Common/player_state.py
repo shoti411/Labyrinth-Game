ec7aeecbed9c6b7bf70d71f369d8c9b3f7071aaf
Dylan Sapienza <dsap@tutamail.com>
2022-10-27 03:03:50 -0400

    update

--- Maze/Common/player_state.py
+++ Maze/Common/player_state.py
@@ -1,5 +1,6 @@
 from gems import Gem
 from tile import Tile
+from coordinate import Coordinate
 
 class Player:
     """
@@ -29,10 +30,8 @@ class Player:
             raise ValueError('Home must be of type Tile')
         if not isinstance(self.__goal, type(Tile())):
             raise ValueError('Goal must be of type Gem')
-        if not(len(self.__position) == 2
-               and isinstance(self.__position[0], int)
-               and isinstance(self.__position[1], int)):
-            raise ValueError('Position must be an array of length 2 with integer values.')
+        if not(isinstance(self.__coordinate, Coordinate)):
+            raise ValueError('coordinate must be an array of length 2 with integer values.')
 
     def get_avatar(self):
         return self.__avatar
@@ -44,7 +43,7 @@ class Player:
         return self.__goal
 
     def get_position(self):
-        return self.__position
+        return self.__coordinate
 
-    def set_position(self, x, y):
-        self.__position = (x, y)
+    def set_position(self, coordinate):
+        self.__coordinate = coordinate
