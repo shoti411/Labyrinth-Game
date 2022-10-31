6c64c84357c49589dff4676961ea0e665dd8728f
Dylan Sapienza <dsap@tutamail.com>
2022-10-31 16:14:47 -0400

    Update Doc

--- Maze/Common/coordinate.py
+++ Maze/Common/coordinate.py
@@ -2,9 +2,9 @@ import math
 
 class Coordinate:
     """
-    Coordinate represents an X,Y location in the Maze Game. 
+    Coordinate represents an X, Y location in the Maze Game. 
     X, Y must be either -1, -1 or both positive ints. 
-    -1, -1 represents the location of an extra Tile.
+    -1, -1 represents a location off the board.
     0, 0 represents the top-most left-most position.
     """
     
@@ -14,12 +14,13 @@ class Coordinate:
         self.__y = y
 
     def __check_coordinate(self,x,y):
+        if (not isinstance(x, int)) or (not isinstance(y, int)):
+            raise ValueError('A Coordiante require X and Y to be integers.')
         if ((x == -1 and y != -1) or (x != -1 and y == -1)):
             raise ValueError('A Coordinate must have both X and Y as -1 or they must both be positive.')
         if ((x != -1 and y != -1) and (x < 0 or y < 0)):
             raise ValueError('A Coordinate must have both X and Y as -1 or they must both be positive.')
-        if (not isinstance(x, int)) or (not isinstance(y, int)):
-            raise ValueError('A Coordiante require X and Y to be integers.')
+        
 
     def getX(self):
         return self.__x
