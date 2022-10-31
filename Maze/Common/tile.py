38f3764aba16a45b3330022fe1d9af299b3ccf87
Dylan Sapienza <dsap@tutamail.com>
2022-10-31 16:22:17 -0400

    Update tile.py

--- Maze/Common/tile.py
+++ Maze/Common/tile.py
@@ -6,36 +6,43 @@ from directions import Direction
 
 class Tile:
     """ 
-    Represents a tile in a board game with possible paths to UP, DOWN, LEFT, and/or RIGHT."""
+    Represents a tile in a board game with possible paths to UP, DOWN, LEFT, and/or RIGHT.
+    """
 
     __acceptable_paths = ['│', '─', '┐', '└', '┌', '┘', '┬', '├', '┴', '┤', '┼']
     __rotation_mapping = [['│', '─'], ['┐', '┌', '└', '┘'], ['┬', '├', '┴', '┤'], ['┼']]
 
     def __init__(self, path_code=False, gems=[]):
         """
-        Constructs a Tile. By default, path_code False and is random.
+        Constructs a Tile. By default, path_code False and gems is an empty list and both will be randomised.
 
         :param: path_code (string): A acceptable string.
             A string is acceptable if it is one of ['│', '─', '┐', '└', '┌', '┘', '┬', '├', '┴', '┤', '┼'].
+        :param: gems (list<Gem>): list of gems.
         """
 
         if not path_code:
             path_code = random.choice(self.__acceptable_paths)
-        if path_code not in self.__acceptable_paths:
-            raise ValueError(f'Given path_code must be in: {self.__acceptable_paths}')
         if not gems:
             gems = random.sample(list(Gem), 2)
+
+        if path_code not in self.__acceptable_paths:
+            raise ValueError(f'Given path_code must be in: {self.__acceptable_paths}')
         for gem in gems:
             if not isinstance(gem, Gem):
                 raise ValueError(f'Given gems must contain only type Gem.')
+        
         self.__gems = gems
         self.__path_code = path_code
 
+
     def get_path_code(self):
-        return copy.deepcopy(self.__path_code)
+        return self.__path_code
+
 
     def get_gems(self):
         return self.__gems
+
 
     def get_paths(self):
         """
@@ -56,7 +63,8 @@ class Tile:
         return directions
 
     def rotate(self, degrees):
-        """ Rotates the tile based on a given number of degrees
+        """
+        Rotates the tile based on a given number of degrees
         
         :param: degrees (int): degrees is an int that represents the number of degrees to rotate by. 
                                degrees can be positive or negative but must be a multiple of 90.
@@ -70,12 +78,15 @@ class Tile:
                 index = rotation_map.index(self.get_path_code())
                 self.__path_code = rotation_map[(index + rotation_steps) % len(rotation_map)]
 
+
     def __str__(self):
         return self.__path_code
 
+
     def __eq__(self, other):
-        if not isinstance(other, self.__class__):
+        if not isinstance(other, Tile):
             return False
+        
         return other.get_path_code() == self.__path_code \
             and all([x in self.__gems for x in other.get_gems()]) \
             and all([x in other.get_gems() for x in self.get_gems()]) \
